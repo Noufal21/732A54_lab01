151d94b43e42d4a42b95767d42ce67f4cc3e9ac4
Work <noufalsiddiqui@gmail.com>
2020-05-29 12:19:26 +0200

    fix changes

--- Lab1.py
+++ Lab1.py
@@ -59,22 +59,24 @@ station_maximum_temperature = station_maximum_temperature.reduceByKey(lambda x,y
 station_maximum_temperature = station_maximum_temperature.filter(lambda x:float(x[1])>= 25 and float(x[1]) <=30)
 
 station_maximum_precipitation = precipitation_lines.map(lambda x: ((x[1][0:4], x[1][5:7],x[1][8:10],x[0]),float(x[3])))
-station_maximum_precipitation = station_maximum_precipitation.reduceByKey(lambda x,y: max(x,y))
-station_maximum_precipitation = station_maximum_precipitation.filter(lambda x: float(x[1])>=10 and float(x[1])<=200)
+station_maximum_precipitation = station_maximum_precipitation.reduceByKey(lambda x,y: x+y)
 station_maximum_precipitation_mapped = station_maximum_precipitation.map(lambda x: (x[0][3],x[1]))
+station_maximum_precipitation_mapped = station_maximum_precipitation_mapped.reduceByKey(lambda x,y: max(x,y))
+station_maximum_precipitation_mapped = station_maximum_precipitation_mapped.filter(lambda x: float(x[1]) >10 and float(x[1]) <=200)
 join_keys = station_maximum_temperature.join(station_maximum_precipitation_mapped).map(lambda x: ((x[0],x[1][0]),[x[1][1]]))
 join_keys = join_keys.reduceByKey(lambda x,y: x+y)
 join_keys.saveAsTextFile("BDA/question4")
 
 
-station_precipitation = precipitation_lines.map(lambda x: (     (x[1][0:4], x[1][5:7],x[0]),   (float(x[3]),1)   ))
+station_precipitation = precipitation_lines.map(lambda x: (     (x[1][0:4], x[1][5:7],x[0]),   float(x[3])   ))
 station_precipitation = station_precipitation.filter(lambda x: int(x[0][0])>=1993 and int(x[0][0])<=2016)
-station_sum_precipitation = station_precipitation.reduceByKey(lambda x,y:(x[0]+y[0],x[1]+y[1]))
-station_average_precipitation = station_sum_precipitation.map(lambda x: (x[0],x[1][0]/x[1][1]))
+station_sum_precipitation = station_precipitation.reduceByKey(lambda x,y:x+y)
+
+#station_average_precipitation = station_sum_precipitation.map(lambda x: (x[0],x[1][0]/x[1][1]))
 
 
 ostergotland_stations = list(ostergotland_lines.map(lambda line: line[0]).collect())
-ostergotland_stations_average_precipitation = station_average_precipitation.filter(lambda x: x[0][2] in ostergotland_stations )
+ostergotland_stations_average_precipitation = station_sum_precipitation.filter(lambda x: x[0][2] in ostergotland_stations )
 
 ostergotland_stations_average_precipitation = ostergotland_stations_average_precipitation.map(lambda x: ((x[0][0],x[0][1]), (x[1],1)))
 ostergotland_stations_average_precipitation = ostergotland_stations_average_precipitation.reduceByKey(lambda x,y:(x[0]+y[0],x[1]+y[1]))
